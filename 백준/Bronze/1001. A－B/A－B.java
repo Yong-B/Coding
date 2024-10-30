6a27a0363f89f85329a2ccbf087d5339f5712055
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2024-10-30 23:56:10 +0900

    [Bronze V] Title: A-B, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-        System.out.println(A-B);
  
-		in.close();
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+   
+        while(A<=0 || A>=10 || B<=0 || B>=10) {
+            A = scan.nextInt();
+            B = scan.nextInt();            
+        }
+        
+        System.out.println(A-B);
     }
 }
