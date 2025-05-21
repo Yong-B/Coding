b157596d48600573764b445368c758668afffd48
최용범 <72883647+Yong-B@users.noreply.github.com>
2025-05-21 22:53:24 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,14 @@
+paper = [[0] * 100 for _ in range(100)]  
+
+n = int(input())
+for _ in range(n):
+    x, y = map(int, input().split())
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            paper[i][j] = 1  
+
+result = 0
+for row in paper:
+    result += row.count(1)
+
+print(result)
