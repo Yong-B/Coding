bd424ac57eccaaf49e7e12e5ea307d51a6ebdbf9
최용범 <72883647+Yong-B@users.noreply.github.com>
2025-05-26 13:39:27 +0900

    [Bronze II] Title: 소수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,20 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i ==0:
+            return False
+    return True
+
+M =int(input())
+N = int(input())
+primes = []
+for num in range(M, N+1):
+    if is_prime(num):
+        primes.append(num)
+    
+if primes:
+    print(sum(primes))
+    print(min(primes))
+else:
+    print(-1)
