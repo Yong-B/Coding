e381df3c2c5dcf1cc825ba4d97dc9d868e913c92
최용범 <72883647+Yong-B@users.noreply.github.com>
2025-08-07 19:29:31 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 428 ms, Memory: 44928 KB -BaekjoonHub

---
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] diffs = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            diffs[i] = B - A;
+        }
+
+        Arrays.sort(diffs);
+
+        int X = diffs[K - 1];
+        System.out.println(Math.max(0, X));
+    }
+}
